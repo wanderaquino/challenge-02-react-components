6132dc83b5168403c535656f80a7b922282ce1ec
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-12 01:57:07 -0300

    Adding sidebar component implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
 import { MovieCard } from './components/MovieCard';
 
 // import { SideBar } from './components/SideBar';
@@ -12,6 +11,7 @@ import './styles/global.scss';
 
 import './styles/sidebar.scss';
 import './styles/content.scss';
+import { SideBar } from './components/SideBar';
 
 interface GenreResponseProps {
   id: number;
@@ -60,26 +60,10 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <nav className="sidebar">
-        <span>Watch<p>Me</p></span>
-
-        <div className="buttons-container">
-          {genres.map(genre => (
-            <Button
-              key={String(genre.id)}
-              title={genre.title}
-              iconName={genre.name}
-              onClick={() => handleClickButton(genre.id)}
-              selected={selectedGenreId === genre.id}
-            />
-          ))}
-        </div>
-
-      </nav>
-
+      <SideBar genres={genres} selectedGenreId={selectedGenreId} handleClickButton={handleClickButton}/>
       <div className="container">
         <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+          <span className="category">Categoria:<span> {selectedGenre.title} </span></span>
         </header>
 
         <main>
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface GenreProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface SideBarProps {
+  genres: GenreProps[];
+  handleClickButton(id: number): void;
+  selectedGenreId: number;
+}
+
+export function SideBar(props: SideBarProps) {
+  console.log(props);
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            selected={props.selectedGenreId === genre.id}
+            onClick= {() => props.handleClickButton(genre.id)}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
